ff3dc4801aed2e235756723c9a3601652f504a2a
Raja Yalamanchili <raja.yalamanchili2@gmail.com>
2024-06-26 01:41:56 +0000

    feat: hw6 Q1-Q4 solutions

---
+++ 06-best-practices/hw6_solution.sh
@@ -0,0 +1,18 @@
+
+# Q1 Refactoring
+echo "Q1. How does the if statement that we use for this looks like?: if __name__ == "__main__":"
+
+# Q2 Installing pytest
+echo "Q2. What should be the other file?: __init__.py"
+
+# Q3 Writing first unit test
+echo "Q3. How many rows should be there in the expected dataframe?: 2"
+
+# Q4 Mocking S3 with Localstack 
+
+echo "Q4. In both cases we should adjust commands for localstack. What option do we need to use for such purposes?: --endpoint-url"
+
+# docker-compose up -d
+
+# aws --endpoint-url=http://localhost:4566 s3 mb s3://nyc-duration
+# aws --endpoint-url=http://localhost:4566 s3 ls
